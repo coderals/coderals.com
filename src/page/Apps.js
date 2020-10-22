8a39893eb47a0af9e83c953aaeced355840564db
saricabasak <saricabasak@gmail.com>
2020-10-22 21:42:45 +0300

    new apps added!

--- src/page/Apps.js
+++ src/page/Apps.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import scrumate from './../image/scrumate.png';
 import keyholder from './../image/keyholder.png';
+import recycleearth from './../image/recycleearth.png';
+import restrospective from './../image/restrospective.png';
 import { Apple, Shop, GitHub } from '@material-ui/icons';
 import { Box, Card, CardMedia, IconButton } from '@material-ui/core';
 
@@ -16,13 +18,13 @@ export default class Apps extends React.Component {
                 component="img"
                 image={scrumate}
               />
-              <IconButton aria-label="appStore">
+              <IconButton aria-label="appStore" href="https://apps.apple.com/tr/app/scrumate/id1495587600">
                 <Apple/>
               </IconButton>
-              <IconButton aria-label="googlePlay">
+              <IconButton aria-label="googlePlay" href="https://play.google.com/store/apps/details?id=com.codecave.tech.scrumate&gl=TR">
                 <Shop/>
               </IconButton>
-              <IconButton aria-label="gitHub">
+              <IconButton aria-label="gitHub" href="https://github.com/Erkanerkisi/scrumate">
                 <GitHub/>
               </IconButton>
             </Card>
@@ -33,13 +35,49 @@ export default class Apps extends React.Component {
                 component="img"
                 image={keyholder}
               />
-              <IconButton aria-label="appStore">
+              <IconButton aria-label="appStore" href="https://apps.apple.com/gb/app/key-holder/id1501233031?l=tr">
                 <Apple/>
               </IconButton>
-              <IconButton aria-label="googlePlay">
+              <IconButton aria-label="googlePlay" href="https://play.google.com/store/apps/details?id=com.coderals.keyholder&gl=TR">
                 <Shop/>
               </IconButton>
               <IconButton aria-label="gitHub" href="https://github.com/coderals/keyholder/">
+                <GitHub/>
+              </IconButton>
+            </Card>
+          </Box>
+        </Box>
+        <Box display="flex" flexDirection="row" justifyContent="space-between" paddingTop="5%">
+          <Box width={2/5}>
+            <Card>
+              <CardMedia
+                  component="img"
+                  image={restrospective}
+              />
+              <IconButton aria-label="appStore" href="https://apps.apple.com/tr/app/retrospectiveapp/id1536191939?l=tr">
+                <Apple/>
+              </IconButton>
+              <IconButton aria-label="googlePlay" href="https://play.google.com/store/apps/details?id=com.coderals.retrospective&gl=TR">
+                <Shop/>
+              </IconButton>
+              <IconButton aria-label="gitHub" href="https://github.com/Erkanerkisi/retrospective/">
+                <GitHub/>
+              </IconButton>
+            </Card>
+          </Box>
+          <Box width={2/5}>
+            <Card>
+              <CardMedia
+                  component="img"
+                  image={recycleearth}
+              />
+              <IconButton aria-label="appStore">
+                <Apple/>
+              </IconButton>
+              <IconButton aria-label="googlePlay" href="https://play.google.com/store/apps/details?id=com.coderals.recycleearth&gl=TR">
+                <Shop/>
+              </IconButton>
+              <IconButton aria-label="gitHub" href="https://github.com/Erkanerkisi/Recycle-Earth">
                 <GitHub/>
               </IconButton>
             </Card>
